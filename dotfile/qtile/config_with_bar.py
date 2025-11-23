2443b36580d882eee9dfb643d998066ac3c01df1
Sylvain Alain <d2racing911@gmail.com>
2025-11-23 18:20:51 -0500

    Create config_with_bar.py

---
+++ dotfile/qtile/config_with_bar.py
@@ -0,0 +1,12 @@
+colors = {
+    "bg": "#1e1e2e",
+    "bg_alt": "#181825",
+    "fg": "#cdd6f4",
+    "fg_alt": "#a6adc8",
+    "blue": "#89b4fa",
+    "mauve": "#cba6f7",
+    "pink": "#f5c2e7",
+    "red": "#f38ba8",
+    "yellow": "#f9e2af",
+    "green": "#a6e3a1",
+}
